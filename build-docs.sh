0fb250b7874ba4a7f6566efb145e9253b784eb8c
Ari Nykänen <ari.o.nykanen@gmail.com>
2021-02-17 15:21:47 +0200

    Removed _index.md building from build-docs.sh. Moved it to docs repo instead.

--- build-docs.sh
+++ build-docs.sh
@@ -1,18 +1,8 @@
-DOCS=docs
-SNIPPETS=docs/doc-snippets
-BUILDS=docs/doc-build
+SNIPPETS=docs/snippets
 
 # ----------- Build -----------------
 TARGET=README.md
 
 cat $SNIPPETS/$TARGET.header > $TARGET
-cat $DOCS/react-ui.md >> $TARGET
+cat index.md >> $TARGET
 cat $SNIPPETS/$TARGET.footer >> $TARGET
-
-# ----------- Build -----------------
-TARGET=_index.md
-
-cat $SNIPPETS/$TARGET.header > $BUILDS/$TARGET
-cat $DOCS/react-ui.md >> $BUILDS/$TARGET
-cat $SNIPPETS/$TARGET.footer >> $BUILDS/$TARGET
-
