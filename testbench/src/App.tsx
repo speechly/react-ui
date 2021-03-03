766de2ff77e3714564648d6f65db4f535e38e836
Ari Nykänen <ari.o.nykanen@gmail.com>
2021-03-03 12:08:36 +0200

    testbench app accepts a query parameter: '?appId=staging:xxxxx'

--- testbench/src/App.tsx
+++ testbench/src/App.tsx
@@ -10,14 +10,34 @@ import {
 //} from "@speechly/react-ui";
 // Run `sh initialize.sh` in the parent directory and uncomment this import to use local linked code.
 } from "./@speechly/react-ui";
+import QueryString from "query-string";
 
 export default function App() {
-  const appId = process.env.REACT_APP_APP_ID ?? "define-your-app-id-here";
-  console.log("Using appId:", appId);
+  // http://localhost:3000/?appId=staging:nnnnn
+
+  const queryAppIdParts = (QueryString.parse(window.location.search).appId as string)?.split(":") || [];
+
+  const appId = ( () => {
+    if (queryAppIdParts.length > 0) return queryAppIdParts.slice(-1)[0];
+    if (!process.env.REACT_APP__APP_ID) throw Error("App ID not defined");
+    return process.env.REACT_APP__APP_ID;
+  })();
+
+  const LoginUrl = queryAppIdParts.length > 0 && queryAppIdParts[0] === "staging" ? "https://staging.speechly.com/login" : process.env.REACT_APP__SPEECHLY_LOGIN_URL;
+  const ApiUrl = queryAppIdParts.length > 0 && queryAppIdParts[0] === "staging" ? "wss://staging.speechly.com/ws/v1" : process.env.REACT_APP__SPEECHLY_API_URL;
+
+  console.log("appId:", appId);
+  console.log("ApiUrl:", ApiUrl);
+  console.log("LoginUrl:", LoginUrl);
 
   return (
     <div className="App">
-      <SpeechProvider appId={appId} language="en-US">
+      <SpeechProvider
+        appId={appId}
+        language="en-US"
+        loginUrl={LoginUrl}
+        apiUrl={ApiUrl}
+      >
         <BigTranscriptContainer>
           <BigTranscript />
           <Notifications/>
